1f3f4d45a9783ed4aedc0df1025c5eabb4a51659
Shailesh07m <shaileshmourya007@gmail.com>
2026-01-03 21:01:30 +0530

    Create wordlist for Hangman game
    Added a list of words for the Hangman game.

---
+++ Handman Game Project/wordlist.py
@@ -0,0 +1,12 @@
+#words for hangman game
+words =  (
+    "cat", "dog", "cow", "buffalo", "goat", "sheep", "horse", "donkey", "pig",
+    "rabbit", "rat", "mouse",
+    "lion", "tiger", "elephant", "leopard", "bear", "wolf", "fox",
+    "deer", "giraffe", "zebra", "monkey",
+    "camel", "kangaroo", "panda",
+    "chicken", "duck", "goose", "pigeon", "crow", "parrot", "peacock", "eagle",
+    "fish", "goldfish", "shark", "whale", "dolphin",
+    "snake", "lizard", "crocodile", "turtle", "frog"
+)
+
